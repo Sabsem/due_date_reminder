5475439a22f417d09883c0acc95e2ffa47e7e155
Ilya IL <ilya-il@users.noreply.github.com>
2019-02-07 11:38:04 +0300

    Support Redmine 4.x

--- lib/reminder/settings_controller_patch.rb
+++ lib/reminder/settings_controller_patch.rb
@@ -4,7 +4,7 @@ module Reminder
       base.class_eval do
         # Same as typing in the class.
         unloadable # Send unloadable so it will not be unloaded in development.
-        before_filter :check_reminder_input, :only => :plugin
+        before_action :check_reminder_input, :only => :plugin
 
         def check_reminder_input
           if request.post? and params[:id] == 'due_date_reminder' and
